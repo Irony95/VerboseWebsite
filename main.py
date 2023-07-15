493d561d784ef8b14e815997f0bb59af00f9110c
Leong Yi Wei <leongyiwei1212@gmail.com>
2023-07-15 22:27:09 +0800

    added upload functionalitiy

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request, flash, redirect, Response, render_template
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('home.html')
 
-@app.route('/upload')
+@app.route('/upload', methods=['POST', 'GET'])
 def upload():
-   return render_template('upload.html')
+    if request.method == "GET":
+        return render_template('upload.html')
+    elif request.method == "POST":
+        if 'fileUpload' in request.files:
+            #This is the actual pdf
+            file = request.files['fileUpload']
+            if file and file.filename != '':
+                file.save(os.path.join('./sentPDFs', file.filename))
 
+        return render_template('learn.html')
 @app.route('/learn')
 def learn():
     return render_template('learn.html')
