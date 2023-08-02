6e7750714332a2c495b29ecab842cf6044e511d2
asha <85682232+asha689@users.noreply.github.com>
2023-08-03 00:22:11 +0800

    Update generate_flashcards.py

--- generate_flashcards.py
+++ generate_flashcards.py
@@ -1,61 +1,37 @@
-## make sure to import nltk and the downloads listed below
-### nltk.download('punkt')
-### nltk.download('stopwords')
-### nltk.download('wordnet')
-
-
-import json
-import nltk
+import os
+import pickle
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 from nltk.corpus import wordnet
 
-def get_flashcards(text, num_keywords = 12):  ## passes the text #you can get the no.of keywords from the user input also 
+def get_keywords_with_definitions(textfile, num_keywords=12):
 
-    def get_clean_tokens(text):
-        tokens = word_tokenize(text.lower())
-        stop_words = set(stopwords.words('english'))
-        clean_tokens = [token for token in tokens if token.isalnum() and token not in stop_words]
-        return clean_tokens
+    with open(os.getcwd() + "\\" + "\\pickle_notes\\" + textfile + "_pickle", "rb") as fp:  # Unpickling
+        text = pickle.load(fp)
 
-    def get_keywords_with_definitions(text, num_keywords = 12):
-        tokens = get_clean_tokens(text)
-        freq_dist = FreqDist(tokens)
-        keywords = freq_dist.most_common(num_keywords)
+    tokens = get_clean_tokens(text)
+    freq_dist = FreqDist(tokens)
+    keywords = freq_dist.most_common(num_keywords)
 
-        flashcards = []
-        for keyword, frequency in keywords:
-            synsets = wordnet.synsets(keyword)
-            if synsets:
-                definition = synsets[0].definition()  
-                flashcards.append((keyword, frequency, definition))
-            else:
-                flashcards.append((keyword, frequency, "No definition found."))
-
-        return flashcards
-
-    flashcards = get_keywords_with_definitions(text, num_keywords)
-
-    # Save flashcards to a JSON file
-    output_file = "Generatedflashcards.json"
-    with open(output_file, "w", encoding="utf-8") as json_file:
-        json.dump(flashcards, json_file, ensure_ascii=False, indent=4)
+    flashcards = []
+    for keyword, frequency in keywords:
+        synsets = wordnet.synsets(keyword)
+        if synsets:
+            definition = synsets[0].definition()
+            print(keyword)
+            flashcards.append((keyword, frequency, definition))
+        else:
+            flashcards.append((keyword, frequency, "No definition found."))
 
     return flashcards
 
-
-
-    ### code to recieve the flashcard from main
-    #   get_flashcards = get_flashcards(text)
-
-    ## recieve with the number of times teh word has appeared in text 
-    ##  print("Keywords with Definitions with the no.of times it appeared in the text:")
-    ##  for keyword, frequency, definition in extracted_keywords: 
-    ##  print(f"{keyword}: {frequency} - Definition: {definition}")
-
-     ## recieve without frequency
-    ##  print("Keywords with Definitions with the no.of times it appeared in the text:")
-    ##  for keyword, frequency, definition in extracted_keywords: 
-    ##  print(f"{keyword}: {frequency} - Definition: {definition}")
+def get_clean_tokens(text):
+    print(type(text))
+    for i in range (len(text)):
+        stringText =text[i] + " ";
+    tokens = word_tokenize(stringText.lower())
+    stop_words = set(stopwords.words('english'))
+    clean_tokens = [token for token in tokens if token.isalnum() and token not in stop_words]
+    return clean_tokens
 
